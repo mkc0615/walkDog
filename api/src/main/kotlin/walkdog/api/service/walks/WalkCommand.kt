91ee05c55b0a3699abe100b7e85a7d9370ca44f3
minkee mac <mkc0615@gmail.com>
2025-12-07 00:54:51 +0900

    refactor: walk commands

--- api/src/main/kotlin/walkdog/api/service/walks/WalkCommand.kt
+++ api/src/main/kotlin/walkdog/api/service/walks/WalkCommand.kt
@@ -4,11 +4,12 @@ import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import walkdog.api.domain.walks.model.Walk
 import walkdog.api.domain.walks.model.WalkCoordinates
+import walkdog.api.domain.walks.model.WalkDog
 import walkdog.api.domain.walks.model.dto.WalkCreateParam
 import walkdog.api.domain.walks.model.dto.WalkCreateResponse
 import walkdog.api.domain.walks.model.dto.WalkPositionParam
 import walkdog.api.domain.walks.model.dto.WalkResultParam
-import walkdog.api.domain.walks.model.dto.WalkUpdateParam
+import walkdog.api.domain.walks.repository.WalkDogRepository
 import walkdog.api.domain.walks.repository.WalkRepository
 import walkdog.api.domain.walks.repository.WalkStatRepository
 import walkdog.api.domain.walks.repository.WalkTrackRepository
@@ -18,17 +19,17 @@ import walkdog.api.domain.walks.repository.WalkTrackRepository
 class WalkCommand(
     private val walkRepository: WalkRepository,
     private val walkStatRepository: WalkStatRepository,
-    private val walkTrackRepository: WalkTrackRepository
+    private val walkTrackRepository: WalkTrackRepository,
+    private val walkDogRepository: WalkDogRepository,
 ) {
     fun createWalk(userId: Long, params: WalkCreateParam): WalkCreateResponse {
         val walk = Walk(userId, params)
-        val result = walkRepository.save(walk)
-        // map dogs
-        val dogIds = params.dogIds.toMutableSet()
+        val walkSaved = walkRepository.save(walk)
+        val dogIds = params.dogIds
 
+        mappingWalkDog(walk.id, dogIds)
 
-        // return response
-        return WalkCreateResponse.create(result.id, result.status)
+        return WalkCreateResponse.create(walkSaved.id, walkSaved.status)
     }
 
     fun updateCoordinates(walkId: Long, params: WalkPositionParam) {
@@ -43,15 +44,37 @@ class WalkCommand(
         }
         walk.finish(params.duration, params.distance)
 
-        // TODO: calculate calories for owner and dogs
-        val dogIds: List<Long> = params.dogIds
+        updateWalkStats(appUserId, params)
+    }
 
-        // TODO: calculate calories burned
-        val calories = 0.0
+    private fun mappingWalkDog(walkId: Long, dogIds: List<Long>) {
+        val walkDogs: List<WalkDog> = dogIds.map { dogId ->
+            WalkDog(dogId, walkId)
+        }
+        walkDogRepository.saveAll(walkDogs)
+    }
+
+    private fun updateWalkStats(appUserId: Long, params: WalkResultParam) {
+        val calories =  calculateCalories(params)
+
+        val dogCalories = params.dogIds.map { dogId ->
+            calculateDogCalories(dogId)
+        }
+
         val stats = walkStatRepository.findByAppUserId(appUserId)
         stats.updateCount()
         stats.updateTotals(params.duration, params.distance, calories)
         stats.updateAverages()
+    }
+
+    private fun calculateCalories(params: WalkResultParam): Double {
+        // TODO: calculate calories burned
+        return 0.0
+    }
+
+    private fun calculateDogCalories(dogId: Long): Double {
+        // TODO: calculate dog calories burned
+        return 0.0
     }
 
     fun removeWalk(id: Long) {
